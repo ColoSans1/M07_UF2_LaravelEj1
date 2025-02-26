a69e1e432e03b275205b3c3e2e059a2e9cac3818
ColoSans1 <arnaucolominassans2004@gmail.com>
2025-02-26 17:04:26 +0100

    Change

--- database/seeders/FilmActorSeeder.php
+++ database/seeders/FilmActorSeeder.php
@@ -2,16 +2,29 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class FilmActorSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        $filmIds = DB::table('films')->pluck('id')->toArray();
+        $actorIds = DB::table('actors')->pluck('id')->toArray();
+
+        foreach ($filmIds as $filmId) {
+            $numActors = rand(1, 3);
+            $selectedActors = array_rand($actorIds, $numActors);
+
+            foreach ((array) $selectedActors as $actorIndex) {
+                DB::table('films_actors')->insert([
+                    'film_id' => $filmId,
+                    'actor_id' => $actorIds[$actorIndex],
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        }
     }
 }
